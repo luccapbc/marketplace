bf848941c01bc1981a51026a2ab2c718ece5da12
Dev-Lucca-Poltronieri <lucca41948@fiec.edu.br>
2026-04-16 20:15:57 -0300

    market place

---
+++ market-place/src/components/range/RangeName.jsx
@@ -0,0 +1,15 @@
+import "./RangeName.css";
+
+const RangeName = () => {
+    return (
+        <div class="div-icone">
+            <h2>ON SALE</h2>
+            <div className="icone">
+                <img src="" alt="" />
+            </div>
+           
+        </div>
+    )
+}
+
+export default RangeName;
--- market-place/src/pages/UserPage.jsx
+++ market-place/src/pages/UserPage.jsx
@@ -1,13 +1,16 @@
 import Header from "../components/header/Header";
 import SideBar from "../components/sidebar/SideBar";
 import Card from "../components/card/Card";
+import RangeName from "../components/range/RangeName";
 
 const UserPage = () => {
     return (
         <main>
             <Header></Header>
+            <RangeName></RangeName>
             <SideBar></SideBar>
             <Card></Card>
+           
         </main>
 
     )
